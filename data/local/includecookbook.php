988f7955ebe18b168bdd053fb49c29856021fb7a
Harco Kuppens <h.kuppens@gmail.com>
2024-11-08 10:39:20 +0100

    added more persons

--- data/local/includecookbook.php
+++ data/local/includecookbook.php
@@ -3,3 +3,11 @@
 @include_once("$FarmD/cookbook/containers/containers.php");
 
 
+@include_once("$FarmD/cookbook/containers/personfmt/personfmt.php");
+// $Containers_FormatterFunctions[] = "containerFmtFunc_person";
+#$Containers_ToggleButtonConfig['style']='';
+array_push($WikiLibDirs, new PageStore('$FarmD/cookbook/containers/wiki.d/{$FullName}'));  // lets you open page 'ContainersExample'
+
+
+@include_once("$FarmD/cookbook/imagepopup/imagepopup.php");
+
